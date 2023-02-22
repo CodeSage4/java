7611e0b2a73f6f6207172331b95c4053807fc21d
CodeSage4 <106674125+CodeSage4@users.noreply.github.com>
2023-02-22 12:40:55 +0530

    Add files via upload

---
+++ hello_java/src/Main.java
@@ -0,0 +1,50 @@
+public class Main {
+    public static void main(String[] args) {
+        //first program
+        System.out.print("Hello world! ");
+        System.out.println("code sage");
+        //sout tab
+
+        //variables
+        String name="Varun";
+        String nei="har";
+        String friend=nei;
+
+
+        //types
+
+        //primitive type--
+
+            //byte-> 1byte [-128,127]
+            //short-> 2byte []
+            //int-> 4bytes
+            //long-> 8bytes
+            //float-> 4
+            //double-> 8
+            //char->2
+            //bool-> 1
+
+        byte age =30;
+        int number=1234567890;
+        long num2=12345678900l;//l or L to distinguish from int
+        float num3=321.0f;//f or F to distinguish from double
+        double num4=3212.05;
+        char leter='f';//  ' for char and " for String
+        boolean check=true;
+
+        //non-primitive types
+            //we can do---traversal,andother things
+        String name2 ="Sage";
+        System.out.println(name2.length());
+        String friend2=new String("Akku");
+
+
+
+
+
+    }
+
+
+
+
+}
